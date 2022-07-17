357c4821d5e666d15fdd0c2bd9a7c29ed0bb0ef6
Skewnart <corentinz@outlook.fr>
2022-07-17 23:40:00 +0200

    Realpath for Cygwin

--- getContent.php
+++ getContent.php
@@ -1,19 +1,19 @@
 <?php
-$datadir = '/var/www/csv/data';
+$datadir = '/projets/cygwin/srv/www/htdocs/processing/data';
 $data = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $type = $_POST["type"];
     if (isset($type) && !empty($type)){
         if ($type == "listcsv"){
-            foreach(scandir($datadir) as $value){
+            foreach(scandir(realpath($datadir)) as $value){
                 if (substr($value,-4) === ".csv"){
                     array_push($data, $value);
                 }
             }
         }
         else{
-            if ($file = fopen("$datadir/$type", "r")) {
+            if ($file = fopen(realpath($datadir."/".$type), "r")) {
                 while(($line = fgets($file)) !== false) {
                     $line = trim($line);
                     if ($line != "")
