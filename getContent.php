c222574a66b1c48a84f4ed6a719f9e23d8058216
Skewnart <corentinz@outlook.fr>
2022-07-18 22:08:12 +0200

    Keep only first occurence of a server in a file

--- getContent.php
+++ getContent.php
@@ -14,10 +14,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         }
         else{
             if ($file = fopen(realpath($datadir."/".$type), "r")) {
+            //On prend uniquement la première occurence d'un même serveur (pour éviter les doublons)
+            //On garde donc le nom des serveurs jusqu'à fin de traitement
+                $serv_names = [];
                 while(($line = fgets($file)) !== false) {
                     $line = trim($line);
                     if ($line != "")
-                        array_push($data, explode(",", trim($line)));
+                    {
+                        $srv_name = explode(',',$line)[0];
+                        if (!in_array($srv_name, $serv_names)){
+                            array_push($serv_names, $srv_name);
+                            array_push($data, explode(",", trim($line)));
+                        }
+                    }
                 }
                 fclose($file);
             }
